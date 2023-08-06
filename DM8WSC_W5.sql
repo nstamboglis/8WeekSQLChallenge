4634daf9f8c1856ffa7b8a90c7ebe74ebf5cd639
nstamboglis <31929763+nstamboglis@users.noreply.github.com>
2023-08-06 16:32:29 +0200

    Add files via upload
    Data cleaning & exploration for week 5

--- DM8WSC_W5.sql
+++ DM8WSC_W5.sql
@@ -1,7 +1,7 @@
 -----------------------------------
 -- Data with Danny - W5
 
--- 1. Data cleansing
+-- 0. Data cleansing
 
 SELECT 
 	TO_DATE(week_date, 'dd/mm/yy')::TIMESTAMP::DATE as week_date, 
@@ -13,9 +13,32 @@ SELECT
     	when segment like '%1' then 'Young Adults'
         when segment like '%2' then 'Middle Aged'
         when segment like ANY(ARRAY['%3', '%4']) then 'Retirees'
+        when segment = 'null' then 'Unknown'
      end as age_band,
-    segment,	
+    case
+    	when segment like 'C%' then 'Couples'
+        when segment like 'F%' then 'Families'
+        when segment = 'null' then 'Unknown'
+     end as demographic,
+    case
+    	when segment != 'null' then segment
+        when segment = 'null' then 'Unknown'
+    end as segment,
     customer_type,	
     transactions,	
-    sales
+    sales,
+    round((sales::numeric / transactions::numeric), 2) as avg_transaction 
 FROM data_mart.weekly_sales;
+
+-- 1. Data exploration
+
+-- 1.1 Date of the week
+SELECT
+	extract(dow from tab1.week_date) as day_of_the_week,
+    tab1.week_number
+FROM(
+  SELECT 
+      TO_DATE(week_date, 'dd/mm/yy')::TIMESTAMP::DATE as week_date, 
+      extract(week from TO_DATE(week_date, 'dd/mm/yy')::TIMESTAMP::DATE) as week_number
+  FROM data_mart.weekly_sales
+) tab1;
